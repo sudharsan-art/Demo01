6a2bd4ae9ef1e261627a1ed01e91d12260c8ab0c
sudharsan-art <sudhar704@gmail.com>
2026-03-04 11:53:56 +0530

    Add half-day leave support to Leave V2 workflow

--- src/main/java/com/app/employeedesk/dto/LeaveRequestV2Dto.java
+++ src/main/java/com/app/employeedesk/dto/LeaveRequestV2Dto.java
@@ -2,7 +2,7 @@ package com.app.employeedesk.dto;
 
 import com.app.employeedesk.enumeration.LeaveType;
 import lombok.Data;
-
+import com.app.employeedesk.enumeration.HalfDaySession;
 import java.time.LocalDate;
 
 @Data
@@ -10,5 +10,7 @@ public class LeaveRequestV2Dto {
     private LeaveType leaveType;
     private LocalDate fromDate;
     private LocalDate toDate;
+    private Boolean halfDay;
+    private HalfDaySession halfDaySession;
     private String reason;
 }
